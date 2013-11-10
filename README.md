1114a5a018f35f0c342fd4374f484754b5818208
Taylor Gendron <tay.gendron@gmail.com>
2013-11-10 13:21:31 -0500

    1st

--- README.md
+++
@@ -1,4 +0,0 @@
-rainforest
-==========
-
-Amazon Clone
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rainforest::Application.config.secret_key_base = '82bf26b08d53c102b336616e8956253c72c576827761c777bea4b34c1c888b3c3736f3f19153e717a47f4de46e9ffb5fede95d90bd52a0196d894ac882e1593b'
