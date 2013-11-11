3446d7e451906575a5a084b82c251ba49a54ef71
Taylor Gendron <tay.gendron@gmail.com>
2013-11-11 16:54:25 -0500

    Writing product validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rainforest::Application.routes.draw do
+  resources :products
 
-  get "products/index"
-  get "products/show"
-  get "products/new"
-  get "products/edit"
-  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
